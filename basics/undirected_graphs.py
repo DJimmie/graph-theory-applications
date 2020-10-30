6eb8b98096553013a93e356fb87204b97030fce4
Jim <30752307+DJimmie@users.noreply.github.com>
2020-10-29 19:13:30 -0500

    continued with undirected graph basics

--- basics/undirected_graphs.py
+++ basics/undirected_graphs.py
@@ -1,56 +1,91 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-class graph(nx.Graph):
+class Graphs():
     """Undirected graph"""
     def __init__(self):
-        super().__init__()
-
+        self.G=nx.Graph()
+        
     def node(self,name):
         self.name=name
-        self.n=self.add_node(self.name)
+        self.G.add_node(self.name)
+        print(self.G.nodes())
+
+        # GraphPlot(self.G)
 
     def node_with_attributes(self,parameters):
-        self.n=self.add_nodes_from(parameters)
+        self.G.add_nodes_from(parameters)
 
     def edges(self,e):
         self.e=e
-        self.edge=self.add_edges_from(self.e)
-
+        self.G.add_edges_from(self.e)
+        
     def graph_metrics(self):
+        print(f'Number of edges:{self.G.number_of_edges()}\nNumber of nodes:{self.G.number_of_nodes()}')
+        GraphPlot(self.G)
 
-        print(f'Number of edges:{self.number_of_edges()}\nNumber of nodes:{self.number_of_nodes()}')
+    def get_node_attributes(self):
+        print(self.G.nodes)
 
-class GraphPlot(nx.draw):
+    def get_edge_attributes(self):
+        print(self.G.edges(data=True))
+
+    def add_node_attribute(self,n,k,v):
+        self.G.nodes[n][k]=v
+        print(list(self.G.nodes(data=True)))
+
+    def neigbors(self):
+        pass
+
+
+    def graph_reporting(self,n):
+
+        print(f'{self.G.__contains__(n)}\n{list(self.G.neighbors(n))}')
+
+        print(self.G.degree)
+
+
+
+
+class GraphPlot():
     """Undirected graph"""
-    def __init__(self):
-        super().__init__()
+    def __init__(self,G):
+        self.G=G
+        self.draw_the_graph()
 
-    def draw_the_graph(self,G):
-        self.draw(G)
-        plt.show
+    def draw_the_graph(self):
+        nx.draw(self.G,with_labels=True,node_color='r',edge_color='b')
+        plt.show()
 
 
 ## MAIN:::::::::::::::::::::::::::MAIN:::::::::::::::::::::::::::MAIN
 
-a=graph()
+a=Graphs()
 
 a.node('Jim')
 
 a.node_with_attributes([
     (4, {"color": "red"}),
-    (5, {"color": "green"}),
-])
+    (5, {"color": "green"}),])
 
+a.edges([(1,2),('A','B'),(2,5)])
 
-a.edges([(1, 2), (1, 3)])
-
-a.edges([("a","c"),("c","d"), ("a",1), (1,"d"), ("a",2)])
+a.edges([('C','D',{'color':'purple'})])
 
 a.graph_metrics()
 
+a.get_node_attributes()
+a.get_edge_attributes()
+
+a.add_node_attribute('Jim','age',56)
+
+a.get_edge_attributes()
+
+a.add_node_attribute('Jim','Home','TX')
+a.add_node_attribute(1,'color','blue')
+a.get_edge_attributes()
+
+a.graph_reporting(2)
 
 
-# p=GraphPlot()
 
-# p.draw_the_graph(a) 
