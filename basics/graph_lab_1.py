7c331a47d31072fefaffc6f3258207d39ac88f72
Jim <30752307+DJimmie@users.noreply.github.com>
2021-05-26 23:33:24 -0500

    Building models

--- basics/graph_lab_1.py
+++ basics/graph_lab_1.py
@@ -19,11 +19,18 @@ def get_from_dict(data_dict,graph_type):
 # H=ug.Graphs(graph_type='dig')
 # %%
 
-data={'CELL': {'console': {'attr': 'HasA', 'color': ''}}, 'console': {'monitor': {'attr': 'HasA', 'color': ''}, 'PC': {'attr': 'HasA', 'color': ''}, 'UPS': {'attr': 'HasA', 'color': ''}}, 'monitor': {}, 'PC': {}, 'UPS': {}, 'cameraPC': {'PC': {'attr': 'IsA', 'color': ''}}, 'daqPC': {'PC': {'attr': 'IsA', 'color': ''}}, 'cameraMonitor': {'monitor': {'attr': 'IsA', 'color': ''}}, 'daqMonitor': {'monitor': {'attr': 'IsA', 'color': ''}}}
-H=get_from_dict(data, graph_type='dig')
+data={'018-21': {'2021-06-01': {'attr': 'hasStartDate', 'color': ''}, '2021-06-15': {'attr': 'hasStopDate', 'color': ''}, 'CELL13': {'attr': 'hasTestLocation', 'color': ''}, 'Jim Dowd': {'attr': 'hasTechnician', 'color': ''}, 'Ramsis II': {'attr': 'hasEngineer', 'color': ''}, 'Approved': {'attr': 'hasStatus', 'color': ''}, 'Pending': {'attr': 'hasTestPhase', 'color': ''}, 'Subsea': {'attr': 'hasProductGroup', 'color': ''}, 
+'20 KSI': {'attr': 'hasWorkingPressure', 'color': ''}, '500°F': {'attr': 'hasMaxTemp', 'color': ''}, '20KSI-123456': {'attr': 'hasPressureSensor', 'color': ''}, '20KSI-987654': {'attr': 'hasPressureSensor', 'color': ''}, 'TP-01 Wireline': {'attr': 'hasTestProcedure', 'color': ''}, 'ASSY-01 Wireline': {'attr': 'hasAssyProcedure', 'color': ''}, '018-21 Binder': {'attr': 'hasDocumentation', 'color': ''}, 'Safety Hazards': {'attr': 'hasA', 'color': ''}}, '2021-06-01': {}, '2021-06-15': {}, 'CELL13': {}, 'Jim Dowd': {}, 'Ramsis II': {}, 'Approved': {}, 'Pending': {}, 'Subsea': {}, '20 KSI': {}, '500°F': {}, '20KSI-123456': {'20 KSI': {'attr': 'hasWorkingPressure', 'color': ''}, 'Pressure Transducer': {'attr': 'isA', 'color': ''}}, '20KSI-987654': {'20 KSI': {'attr': 'hasWorkingPressure', 'color': ''}, 'Pressure Transducer': {'attr': 'isA', 'color': ''}}, 'TP-01 Wireline': {'018-21 Binder': {'attr': 'isLocatedIn', 'color': ''}}, 'ASSY-01 Wireline': {'018-21 Binder': {'attr': 'isLocatedIn', 'color': ''}}, '018-21 Binder': {}, 'Safety Hazards': {'018-21 Binder': {'attr': 'isLocatedIn', 'color': ''}}, 'Pressure Transducer': {}}
+
+H=get_from_dict(data, graph_type='ug')
+
+ug.GraphPlot(H)
     
 # %%
 
+# H=nx.MultiGraph()
 
+
+# %%
 
 # %%
